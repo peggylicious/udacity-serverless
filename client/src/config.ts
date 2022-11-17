338428b7ec6b4dc71f392c4bf5f59bbe5d36ebb1
peggylicious <p.asq@live.com>
2022-11-17 10:52:56 +0100

    Severless app created

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'frjuw0hn80';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-wahcj0purvfumqmy.us.auth0.com',
+  clientId: '5Qc645sKw5vlDPPo3NXORPD7sP4ZrBNj',
+  callbackUrl: 'http://localhost:3000/callback'
+};
